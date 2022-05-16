dc95d130e5ebcd5b6f0c872593000b5c6b0e0c15
Michael Kaszynski <mkaszynski2000@gmail.com>
2022-05-16 19:58:33 +0200

    imporoved pool table

--- pool_table.py
+++ pool_table.py
@@ -1,388 +1,145 @@
 import pygame
-import pygame.locals as pg_locals
+import time
 import random
-import time as t
+import math
 
-WELCOME_TXT = 'Hello! Welcome to pool table game! Press enter to start.'
-
-def wrap_text(text, font, colour, x, y, screen, allowed_width):
-    # first, split the text into words
-    words = text.split()
-
-    # now, construct lines out of these words
-    lines = []
-    while len(words) > 0:
-        # get as many words as will fit within allowed_width
-        line_words = []
-        while len(words) > 0:
-            line_words.append(words.pop(0))
-            fw, fh = font.size(' '.join(line_words + words[:1]))
-            if fw > allowed_width:
-                break
-
-        # add a line consisting of those words
-        line = ' '.join(line_words)
-        lines.append(line)
-
-    # now we've split our text into lines that fit into the width, actually
-    # render them
-
-    # we'll render each line below the last, so we need to keep track of
-    # the culmative height of the lines we've rendered so far
-    y_offset = 0
-    for line in lines:
-        fw, fh = font.size(line)
-
-        # (tx, ty) is the top-left of the font surface
-        tx = x
-        ty = y + y_offset
-        y_offset += fh
-
-        font_surface = font.render(line, True, colour)
-        screen.blit(font_surface, (tx, ty))
-
-
-def rand_color():
-    return (random.randint(0, 255),
-            random.randint(0, 255),
-            random.randint(0, 255))
-
-def add_line(screen, text, x, y):
+def add_line(screen, text, x, y, color=(0, 0, 0)):
     # used to print the status of the variables
-    text = font.render(text, True, white) 
+    text = font.render(text, True, color)
     text_rect = text.get_rect()
     text_rect.topleft = (x, y)
     screen.blit(text, text_rect)
 
-
-black = (0, 0, 0)
-white = (255, 255, 255)
-
-TICKS = 60
-vel_y = 0
-
-GRAVITY = -3
-ANTIGRAVITY = 3
-red = 75
-green = 25
-blue = 0
-
 pygame.init()
-font = pygame.font.Font('freesansbold.ttf', 32)
 
+font = pygame.font.Font("freesansbold.ttf", 24)
 
-# set the screen size
-full_screen = True
-if full_screen:
-    info = pygame.display.Info()
-    SCREEN_WIDTH, SCREEN_HEIGHT = info.current_w, info.current_h - 105
-else:
-    SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
+# Set up the drawing window
+screen = pygame.display.set_mode([1200, 600])
 
-screen = pygame.display.set_mode((0, 0))
+balls = [[700, 250, 0, 0, (255, 255, 255), None]]
 
+for i in range(5):
+    for j in range(5):
+        if i > j:
+            balls.append([j*25 + 100, i*25 + 200 - j*25*0.5, 0, 0, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), i + j - 2])
 
-HEIGHT = 40
-GROUND_Y = SCREEN_HEIGHT + HEIGHT + 60
+angle = 0
 
-pos_x = 0.5 * SCREEN_WIDTH
-pos_y = GROUND_Y
-
-my_color = (160, 0, 0)
-my_color2 = (0, 0, 200)
-bounce = 0
-gravity = 1
-friction = 0.1
-pos_x2 = 0.4 * SCREEN_WIDTH
-pos_y2 = GROUND_Y
-jump = 20
-vel_y2 = 0
-vel_x2 = 0
-health = 100
-health2 = 100
-second_time = False
-second_time2 = False
-healing = 0.005
-healing2 = 0.005
-heling = False
-heling2 = False
-strength = False
-strength2 = False
-poison = False
-poison2 = False
-time = 0
-ground_c = 125
-sun_moon = 255
-sun_moon2 = 0
 points = 0
-points2 = 0
 
-def star(posx, posy):
-    he = pygame.Rect(posx, posy, 5, 5)
-    pygame.draw.rect(screen, (255, 255, 255), he)
-        
+holes = [[0, 250], [570, 0], [1170, 570], [1170, 0], [570, 570], [0, 570], [1170, 250]]
 
-# use this clock to limit the update of the game
-clock = pygame.time.Clock()
-
-unn = True
-while unn:
-    screen.fill((120, 0, 170))# make entire screen black
-    add_line(screen, 'Kaszynski studios',
-                     550, 250)
-    pygame.event.poll()
-    pygame.display.update()
-    t.sleep(2)
-    clock.tick(TICKS)
-    unn = False
-
-vel_x = 0
-vel_y = 0
-h = random.randint(0, 1000)
-n = random.randint(0, 1000)
-
-
-first_time = True
+# Run until the user asks to quit
 running = True
 while running:
-    screen.fill((red, green, blue))# make entire screen black
-    
-    
-    
-    # display welcome text
-    if first_time:
-        
-        wrap_text(WELCOME_TXT, font, 'white', 400, 200, screen,
-                  SCREEN_WIDTH // 2)
-
-        pygame.event.poll()
-        keys = pygame.key.get_pressed()
-        pygame.display.update()
-
-        if keys[pg_locals.K_RETURN]:
-            first_time = False
-        
-        continue        
-
-    pygame.display.set_caption('fight game')
-
-    GROUND_Y = SCREEN_HEIGHT - HEIGHT
-    on_ground = pos_y == GROUND_Y
-
-    # update events
+    # Fill the background with white
+    screen.fill((128, 74, 0))
     pygame.event.poll()
-
     keys = pygame.key.get_pressed()
-    
-    if True:
-        if keys[pg_locals.K_d]:
-            vel_x = 10
-        if keys[pg_locals.K_a]:
-            vel_x = -10
-            
-                        
-        if health > 100:
-            health = 100
-        if health > 0:
-            health = health + healing
-        
-                
-        if health2 > 100:
-            health2 = 100
-        if health2 > 0:
-            health2 = health2 + healing2
-            
-        if heling == True:
-            healing = 0.01
-        if heling2 == True:
-            healing2 = 0.01
-            
-        if poison == True:
-            health = health - 0.03
-        if poison2 == True:
-            health2 = health - 0.03
-        
-        if keys[pg_locals.K_k]:
-            vel_x2 = 10
-        if keys[pg_locals.K_h]:
-            vel_x2 = -10
-    
-        # simulate drag
-        vel_x -= friction *vel_x
-        if abs(vel_x) < 0.5:
-            vel_x = 0
-        vel_x2 -= friction *vel_x2
-        if abs(vel_x2) < 0.5:
-            vel_x2 = 0
-            
-        # if pos_x > pos_x2 - 40:
-        #     vel_x2 = vel_x
-        #     vel_x = 0
-        # if pos_x < pos_x2 + 40:
-        #     vel_x2 = vel_x
-        #     vel_x = 0
-        # if pos_x2 > pos_x - 40:
-        #     vel_x = vel_x2
-        #     vel_x2 = 0
-        # if pos_x2 < pos_x + 40:
-        #     vel_x2 = vel_x
-        #     vel_x2 = 0
-            
-        
-        # if pos_y > pos_y2 - 40:
-        #     vel_y2 = vel_y
-        #     vel_y = 0
-        # if pos_y < pos_y2 + 40:
-        #     vel_y2 = vel_y
-        #     vel_y = 0
-        # if pos_y2 > pos_y - 40:
-        #     vel_y = vel_y2
-        #     vel_y2 = 0
-        # if pos_y2 < pos_y + 40:
-        #     vel_y2 = vel_y
-        #     vel_y2 = 0
-        
-    if pos_y < 40 and pos_x == 40:
-        points = points + 1
-        pos_y = 0.5 * SCREEN_HEIGHT
-        pos_x = 0.5 * SCREEN_WIDTH
-    if pos_y2 < 40 and pos_x2 == 40:
-        points2 = points2 + 1
-        pos_y2 = 0.5 * SCREEN_HEIGHT
-        pos_x2 = 0.4 * SCREEN_WIDTH
-        
-    pos_x += vel_x
-    
-    pos_x2 += vel_x2
+    mx, my = pygame.mouse.get_pos()
+    mouse_held = pygame.mouse.get_pressed()
 
-    # bounds checking
-    if pos_x < 40:
-        pos_x = 40
-        vel_x = vel_x * -1
-    if pos_x > SCREEN_WIDTH - HEIGHT * 2:
-        pos_x = SCREEN_WIDTH - HEIGHT * 2
-        vel_x = vel_x * -1
-        
-    if pos_x2 < 40:
-        pos_x2 = 40
-        vel_x = vel_x * -1
-    if pos_x2 > SCREEN_WIDTH - HEIGHT * 2:
-        pos_x2 = SCREEN_WIDTH - HEIGHT * 2
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    if keys[pygame.K_w]:
+        angle += 0.03
+    if keys[pygame.K_s]:
+        angle -= 0.03
+    
+    for i in balls:
+        for j in balls:
+            if i == j:
+                continue
+            if j[0] > i[0] - 20 and j[0] < i[0] + 20:
+                if j[1] > i[1] - 20 and j[1] < i[1] + 20:
+                    if random.random() > 0.75:
+                         n = i[2]
+                         m = i[3]
+                         a = j[2]
+                         b = j[3]
+                         i[2] = a + random.random()*i[2] - 0.5*i[2]
+                         j[2] = n + random.random()*i[2] - 0.5*i[2]
+                         i[3] = b + random.random()*i[2] - 0.5*i[2]
+                         j[3] = m + random.random()*i[2] - 0.5*i[2]
+    
+    if keys[pygame.K_e]:
+        for i in balls:
+            if i[4] == (255, 255, 255):
+                if abs(i[2]) < 0.3 and abs(i[3]) < 0.3:
+                    i[2] = math.sin(angle)*25
+                    i[3] = math.cos(angle)*25
+    
+    for i in balls:
+        for j in holes:
+            if j[0] > i[0] - 30 and j[0] < i[0] + 20:
+                if j[1] > i[1] - 30 and j[1] < i[1] + 20:
+                    if not i[4] == (255, 255, 255):
+                        balls.remove(i)
+                        points += i[5]
+                    else:
+                        i[0] = 700
+                        i[1] = 250
+                        points -= 1
+    
+    for i in balls:
+        i[2] *= 0.975
+        i[3] *= 0.975
+        i[0] += i[2]
+        i[1] += i[3]
+        if i[0] < 20:
+            i[0] = 20
+            i[2] *= -1
+        if i[1] < 20:
+            i[1] = 20
+            i[3] *= -1
+        if i[0] > 1160:
+            i[0] = 1160
+            i[2] *= -1
+        if i[1] > 560:
+            i[1] = 560
+            i[3] *= -1
+    
+    map1 = pygame.Rect(20, 20, 1160, 560)
+    pygame.draw.rect(screen, (0, 128, 0), map1)
+    
+    for i in holes:
+        map1 = pygame.Rect(i[0], i[1], 30, 30)
+        pygame.draw.rect(screen, (0, 0, 0), map1)
+    
+    for i in balls:
+        if not i[5] == None:
+            if i[5] >= 0:
+                map1 = pygame.Rect(i[0], i[1], 20, 20)
+                pygame.draw.rect(screen, i[4], map1)
+            else:
+                map1 = pygame.Rect(i[0], i[1], 20, 20)
+                pygame.draw.rect(screen, (0, 0, 0), map1)
+        else:
+            map1 = pygame.Rect(i[0], i[1], 20, 20)
+            pygame.draw.rect(screen, i[4], map1)
+        if not i[5] == None:
+            if i[5] >= 0:
+                add_line(screen, str(i[5]), i[0], i[1])
+            else:
+                add_line(screen, str(i[5]), i[0], i[1], color=(255, 255, 255))
+    
+    add_line(screen, f'points: {points}', 0, 0)
+    
+    for i in balls:
+        if abs(i[2]) < 0.3 and abs(i[3]) < 0.3:
+            if i[4] == (255, 255, 255):
+                pygame.draw.line(screen, (128, 74, 0), (i[0], i[1]), (i[0] + math.sin(angle)*-400, i[1] + math.cos(angle)*-400), width=10)
+                pygame.draw.line(screen, (0, 74, 128), (i[0], i[1]), (i[0] + math.sin(angle)*400, i[1] + math.cos(angle)*400), width=3)
     
     
-    if keys[pg_locals.K_q]:
-        running = False
-    if keys[pg_locals.K_w]:
-        vel_y -= 5
-            
-    if keys[pg_locals.K_u]:
-        vel_y2 -= 5
-            
-    if health2 < 0:
-        second_time2 = True
-    if health < 0:
-        second_time = True
-    
-    # falls to the roof
-    if keys[pg_locals.K_s]:
-        vel_y = 10
-    if keys[pg_locals.K_j]:
-        vel_y2 = 10
-        
-    if vel_y > 0:
-        vel_y = vel_y - friction * vel_y
-    if vel_y < 0:
-        vel_y = vel_y - friction * vel_y
-    if vel_y2 > 0:
-        vel_y2 = vel_y2 - friction * vel_y2
-    if vel_y2 < 0:
-        vel_y2 = vel_y2 - friction * vel_y2
-    
-    # update position using velocity
-    pos_y += vel_y
-    
-    pos_y2 += vel_y2
-    
-    
-    if pos_y < 40 and pos_x != 0:
-        pos_y = 40
-        vel_y *= -1  # make bounce
-    if pos_y > GROUND_Y:
-        pos_y = GROUND_Y
-        # must reset velocity as well
-        vel_y *= -1  # make bounce
-        
-    if pos_y2 < 40:
-        pos_y2 = 40
-        vel_y2 *= -1  # make bounce
-    if pos_y2 > GROUND_Y:
-        pos_y2 = GROUND_Y
-        # must reset velocity as well
-        vel_y2 *= -1  # make bounce
-    
-    # simulate drag
-    # if abs(vel_y) < 3.0:
-    #     vel_y = 3.0
-    
-    # if abs(vel_y2) < 3.0:
-    #     vel_y2 = 3.0
-    
-    ju = time * 0.5
-    
-        
-    # print out the current variables
-    # add_line(screen, f'vel 1: {vel_y :.0f}',
-    #           0, 0)
-    # add_line(screen, f'vel 2: {vel_y2 :.0f}',
-    #           0, 45)
-    # add_line(screen, f'time: {time :.0f}',
-    #             0, 90)
-        
-    # if second_time == True:
-    #     if second_time2 == False:
-    #         add_line(screen, 'player 2 won! press enter to end the game or press p to play again',
-    #                  200, 250)
-    #         if keys[pg_locals.K_RETURN]:
-    #             running = False
-    #         if keys[pg_locals.K_p]:
-    #             health = 100
-    #             health2 = 100
-    #             pos_x = 0.5 * SCREEN_WIDTH
-    #             pos_x2 = 0.4 * SCREEN_WIDTH
-    #             second_time = False
-    
-    # if second_time2 == True:
-    #     if second_time == False:
-    #         add_line(screen, 'player 1 won! press enter to end the game or press p to play again',
-    #                  200, 250)
-    #         if keys[pg_locals.K_RETURN]:
-    #             running = False
-    #         if keys[pg_locals.K_p]:
-    #             health = 100
-    #             health2 = 100
-    #             pos_x = 0.5 * SCREEN_WIDTH
-    #             pos_x2 = 0.4 * SCREEN_WIDTH
-    #             second_time2 = False
-                
+    time.sleep(1/60)
 
-    ground = pygame.Rect(40, 40, SCREEN_WIDTH - 80, SCREEN_HEIGHT - 40)
-    pygame.draw.rect(screen, (0, ground_c, 0 ), ground)
-        
-    
-    if health >= 0:
-        rect = pygame.Rect(pos_x, pos_y, HEIGHT, HEIGHT)
-        pygame.draw.rect(screen, my_color, rect)
-    
-    if health2 >= 0:
-        rect2 = pygame.Rect(pos_x2, pos_y2, HEIGHT, HEIGHT)
-        pygame.draw.rect(screen, my_color2, rect2)
-    
-    hole1 = pygame.Rect(40, 0, HEIGHT, HEIGHT)
-    pygame.draw.rect(screen, (0, 0, 0), hole1)
-    
-    pygame.display.update()
+    # Flip the display
+    pygame.display.flip()
 
-    # wait until next tick
-    clock.tick(TICKS)
-
+# Done! Time to quit.
 pygame.quit()
